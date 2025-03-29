d35d2dca9c920890f858f629af7bd91d7b940713
lukas <lukas.hartwig@gmx.net>
2025-03-29 12:12:42 +0100

    did some more

--- lim.c
+++ lim.c
@@ -20,37 +20,95 @@ typedef struct {
 
 typedef struct {
   uint16_t x, y;
-  uint16_t rows;
+  uint16_t rows, cols;
 } Editor;
 
+int count_rows() {
+  for (int i = 1; i < 100; i++) {
+    printw("%d", i);
+    if (move(i, 0) == ERR) {
+      move(0, 0);
+      return i;
+    }
+  }
+  printf("window has 100 rows ???\n");
+  endwin();
+  exit(0);
+}
+
+int count_cols() {
+  for (int i = 1; i < 500; i++) {
+    if (move(0, i) == ERR) {
+      move(0, 0);
+      return i;
+    }
+  }
+  printf("window has 200 cols ???\n");
+  endwin();
+  exit(0);
+}
+
+int read_file_to_buffer(GapBuffer *g, Editor *e, char* filename) {
+  FILE *file = fopen(filename, "r");
+  if (!file) {
+    exit(1);
+  }
+    
+  char c;
+  for (g->p = 0; (c = fgetc(file)) != EOF; g->p++) {
+    g->buf[g->p] = c;
+    //e->rows += (c == '\n') ? 1 : 0;
+  }
+}
+
+int print_status_line(Editor *e, GapBuffer g, int c) {
+  //move(e.rows - 1, 0);
+  //printw("                                                                                                       ");
+  move(e->rows - 1, 0);
+  attrset(COLOR_PAIR(2));
+  printw("c: %d, e: (%d, %d), f: %d, lines: %d", c, e->y, e->x, g.front, e->rows);
+  attrset(COLOR_PAIR(1));
+  //move(e.y, e.x);
+}
+
+//void die(char *str)
+
 int main(int argc, char **argv) {
- 
+
+  initscr();
+  start_color();
+  
   Editor e = { .rows = 1 };
+
+  e.cols = count_cols();  
+  e.rows = count_rows();
+  
+  // die!!!
+  //printf("rows: %d. cols: %d\n", e.rows, e.cols); endwin(); exit(0);
+
+  if (!has_colors()) {
+    printf("No Colors\n");
+    exit(1);
+  }
+  init_pair(1, COLOR_GREEN, COLOR_BLACK);
+  init_pair(2, COLOR_BLACK, COLOR_GREEN);
+  attrset(COLOR_PAIR(1));
+
   GapBuffer g = { .gap = 10000 };
   g.buf = malloc(10000);
 
-  for (int i = 1; i < argc; i++) {
-    FILE *file = fopen(argv[1], "r");
-    if (!file) {
-      exit(1);
-    }
-    
-    char c;
-    for (g.p = 0; (c = fgetc(file)) != EOF; g.p++) {
-      g.buf[g.p] = c;
-      e.rows += (c == '\n') ? 1 : 0;
-    }
+  if (argc > 1) {
+    read_file_to_buffer(&g, &e, argv[1]);
+    printw(g.buf);
+    move(0, 0);
+    refresh();
   }
-  
-  initscr();
+  print_status_line(&e, g, 0);
+
   raw();
-  //cbreak();
   keypad(stdscr, TRUE);
   noecho();
-
-  printw(g.buf);
-  refresh();
-
+  
   int c;
   while ((c = getch()) != STR_Q) {
     
@@ -91,8 +149,9 @@ int main(int argc, char **argv) {
       g.buf[g.front] = c;
       g.front++;
       e.x += 1;
-      clear();
-      printw(g.buf);
+      addch(c);
+      //clear();
+      //printw(g.buf);
     }
 
     if (c == 127) {
@@ -104,11 +163,7 @@ int main(int argc, char **argv) {
     //clear();
     refresh();
     //printw(g.buf);
-    move(30, 0);
-    printw("                                                                                                       ");
-    move(30, 0);
-    printw("c: %d, e: (%d, %d), f: %d, lines: %d", c, e.y, e.x, g.front, e.rows);
-    move(e.y, e.x);
+    print_status_line(&e, g, c);
   }
   
   clear();
