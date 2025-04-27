6d260c31749b38eed8148eab548541ee6d77877c
lukas <lukas.hartwig@gmx.net>
2025-04-27 11:27:49 +0200

    add deltatime to game

--- game.c
+++ game.c
@@ -2,30 +2,66 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#include <sys/time.h>
+
 
 int main() {
-  int c;
-  system("setfont lat0-08.psfu.gz");
-  //execl("/usr/bin/setfont", "setfont", "default8x16.psfu.gz", NULL);
-  //execl("/usr/bin/setfont", "setfont", "lat0-08.psfu.gz", NULL);
-  //exit(0);
-  // execv("setfont lat0-08.psfu.gz", NULL);
+  WINDOW *textArea;
+  int c, rows, cols;
+  struct timeval tp;
+  unsigned long millis = 1000, old_millis = 1000;
+	unsigned long delta = 1000;
+
+  char buffer[100] = "asdasdasdasd";
+  unsigned int index = 0;
+
+  // system("setfont lat0-08.psfu.gz");
+
   initscr();
-  keypad(stdscr, TRUE);
+  atexit((void*)endwin);
+
+  raw();
+  keypad(textArea, TRUE);
   noecho();
-  while ((c = getch()) != 'q') {
-    switch (c) {
-      case KEY_DOWN:
-        printw ("KEY_DOWN : %d\n", c);
-        break;
-      default:
-        printw ("Tastaturcode %d\n",c);
-    }
-  }
+
+  getmaxyx(stdscr, rows, cols);
+  textArea = newwin(rows, cols, 0, 0);
+
+  millis = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+
+  // mvwprintw(textArea, 0, 0, "%s", buffer);
+  wrefresh(textArea);
+
+  // while ((c = getch()) != 'q') {
+  //   gettimeofday(&tp, NULL);
+  //   millis = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+  //   delta = millis - old_millis;
+
+  //   // switch (c) {
+  //   //   case KEY_DOWN:
+  //   //     printw ("KEY_DOWN : %d <-> %lu\n", c, delta);
+  //   //     break;
+  //   //   default:
+  //   //     printw ("Tastaturcode %d\n",c);
+  //   // }
+
+  //   /** TEST */ 
+
+  //   switch (c) {
+  //     default:
+  //       buffer[index++] = c; 
+  //   }
+
+  //   // wclear(textArea);
+  //   mvwprintw(textArea, 0, 0, "%s", buffer);
+  //   wrefresh(textArea);
+
+  //   old_millis = millis;
+
+  // }
   endwin();
-  //execl("/usr/bin/setfont", "setfont", "default8x16.psfu.gz", NULL);
-  system("setfont default8x16.psfu.gz");
+  // system("setfont default8x16.psfu.gz");
 
   return 0;
 }
-//setfont default8x16.psfu.gz
+
